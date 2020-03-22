9d0bf4ae88c22cf16147ffffdc673136043be239
Joel James <jayfiled@gmail.com>
2020-03-23 07:00:58 +1100

    add a check for authentication when page loads

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -21,6 +21,14 @@ class Inventory extends React.Component {
     owner: null
   };
 
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.authHandler({ user} );
+      }
+    })
+  }
+
   authHandler = async authData => {
     // 1. Look up the current store in the firebase database
 
